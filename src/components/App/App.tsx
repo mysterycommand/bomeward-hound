c218cdc1d83197e86e2bce84e6d244c9bb3ea97a
Matt Hayes <matt@mysterycommand.com>
2020-05-25 16:53:37 -0400

    refactor: component folder pattern

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "../../features/counter/Counter";
+import "./App.css";
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -54,5 +54,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
