c63262dc3bd3a894a81c9e09bc6a157719ce05ef
Matt Hayes <matt@mysterycommand.com>
2020-06-07 23:52:52 -0400

    refactor: make the list of urls more flexible, add some state, effects, click handlers, update css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,42 +1,66 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 
 import styles from './App.module.css';
 
-export const App: FC = () => (
-  <div className={styles.app}>
-    <ol className={styles.doors}>
-      <li className={styles.door}>
-        <iframe
-          className={styles.room}
-          title="https://theweeklyweekly.com/"
-          src="https://theweeklyweekly.com/"
-        />
-      </li>
-      <li className={styles.door}>
-        <iframe
-          className={styles.room}
-          title="https://s4y.us"
-          src="https://s4y.us"
-        />
-      </li>
-      <li className={styles.door}>
-        <iframe
-          className={styles.room}
-          title="https://mysterycommand.com"
-          src="https://mysterycommand.com"
-        />
-      </li>
-      <li className={styles.door}>
-        <iframe
-          className={styles.room}
-          title="https://webglsamples.org/aquarium/aquarium.html"
-          src="https://webglsamples.org/aquarium/aquarium.html"
-        />
-      </li>
-    </ol>
-    <footer className={styles.hud}>
-      <button className={styles.nav}>previous room</button>
-      <button className={styles.nav}>next room</button>
-    </footer>
-  </div>
-);
+const rooms: string[] = [
+  'https://theweeklyweekly.com/',
+  'https://s4y.us',
+  'https://mysterycommand.com',
+  'https://webglsamples.org/aquarium/aquarium.html',
+];
+
+export const App: FC = () => {
+  const doorsRef = useRef<HTMLOListElement>(null);
+  const innerWidthRef = useRef(0);
+
+  const [currentRoom, setCurrentRoom] = useState(0);
+
+  useEffect(() => {
+    const onResize = () => {
+      innerWidthRef.current = window.innerWidth;
+    };
+    onResize();
+
+    window.addEventListener('resize', onResize, { passive: true });
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    doorsRef.current?.scrollTo({
+      left: innerWidthRef.current * currentRoom,
+      behavior: 'smooth',
+    });
+  }, [currentRoom]);
+
+  return (
+    <div className={styles.app}>
+      <ol ref={doorsRef} className={styles.doors}>
+        {rooms.map((room) => (
+          <li key={room} className={styles.door}>
+            <iframe className={styles.room} title={room} src={room} />
+          </li>
+        ))}
+      </ol>
+      <footer className={styles.hud}>
+        <button
+          className={styles.nav}
+          onClick={() => {
+            setCurrentRoom((currentRoom + rooms.length - 1) % rooms.length);
+          }}
+        >
+          previous room
+        </button>
+        <button
+          className={styles.nav}
+          onClick={() => {
+            setCurrentRoom((currentRoom + 1) % rooms.length);
+          }}
+        >
+          next room
+        </button>
+      </footer>
+    </div>
+  );
+};
