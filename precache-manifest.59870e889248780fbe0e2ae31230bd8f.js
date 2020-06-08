d7497dc6961908de9b51de8a5d74fad5c734e8c7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-08 00:23:07 +0000

    Deploy mysterycommand/bomeward-hound to github.com/mysterycommand/bomeward-hound.git:gh-pages

---
+++ precache-manifest.59870e889248780fbe0e2ae31230bd8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa981c8904a741361205a912fd4493a9",
+    "url": "/bomeward-hound/index.html"
+  },
+  {
+    "revision": "e21ab1f67d809d4a9fbb",
+    "url": "/bomeward-hound/static/css/main.cd16a405.chunk.css"
+  },
+  {
+    "revision": "ce37cee15e26e909e768",
+    "url": "/bomeward-hound/static/js/2.a8474ef4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bomeward-hound/static/js/2.a8474ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21ab1f67d809d4a9fbb",
+    "url": "/bomeward-hound/static/js/main.f1969cee.chunk.js"
+  },
+  {
+    "revision": "8e3a527fcf02bdb7e66c",
+    "url": "/bomeward-hound/static/js/runtime-main.e3e4fc63.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/bomeward-hound/static/media/logo.8dad8028.svg"
+  }
+]);
