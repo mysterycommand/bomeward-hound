466c7a97b3bab7194ae08d6c12791ffd1e4a3b2f
Matt Hayes <matt@mysterycommand.com>
2020-06-07 22:58:42 -0400

    feat: add some doors and rooms

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,4 +2,37 @@ import React, { FC } from 'react';
 
 import styles from './App.module.css';
 
-export const App: FC = () => <div className={styles.app}></div>;
+export const App: FC = () => (
+  <div className={styles.app}>
+    <ol className={styles.doors}>
+      <li className={styles.door}>
+        <iframe
+          className={styles.room}
+          title="https://webglsamples.org/aquarium/aquarium.html"
+          src="https://webglsamples.org/aquarium/aquarium.html"
+        />
+      </li>
+      <li className={styles.door}>
+        <iframe
+          className={styles.room}
+          title="https://theweeklyweekly.com/"
+          src="https://theweeklyweekly.com/"
+        />
+      </li>
+      <li className={styles.door}>
+        <iframe
+          className={styles.room}
+          title="https://s4y.us"
+          src="https://s4y.us"
+        />
+      </li>
+      <li className={styles.door}>
+        <iframe
+          className={styles.room}
+          title="https://mysterycommand.com"
+          src="https://mysterycommand.com"
+        />
+      </li>
+    </ol>
+  </div>
+);
